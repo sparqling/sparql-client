572e1ff3aed6b3b53dd4365eb27b91b0302105e6
Hirokazu Chiba <hchiba1@gmail.com>
2023-09-02 04:46:40 +0900

    Use axios for querying

--- bin/sparql-client.js
+++ bin/sparql-client.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
 const fs = require('fs').promises;
+const axios = require('axios');
 const program = require('commander');
 const version = require('../package.json').version;
 
 const opts = program
+  .option('-e, --endpoint <ENDPOINT>', 'target SPARQL endpoint', 'https://spang.dbcls.jp/sparql-test')
+  .option('-f, --format <FORMAT>', 'tsv, json, n-triples (nt), turtle (ttl), rdf/xml (rdfxml), n3, xml, html, text', 'json')
   .version(version)
   .arguments('[SPARQL_FILE]')
   .parse(process.argv)
@@ -13,6 +16,27 @@ const opts = program
 if (program.args.length < 1 && process.stdin.isTTY) {
   program.help();
 }
+
+const acceptHeaderMap = {
+  "xml"      : "application/sparql-results+xml",
+  "json"     : "application/sparql-results+json",
+  "tsv"      : "application/sparql-results+json", // receive as json and format to tsv afterward
+  "text"     : "text/tab-separated-values",
+  "csv"      : "text/csv",
+  "n-triples": "text/plain",
+  "nt"       : "text/plain",
+  "n3"       : "text/rdf+n3",
+  "html"     : "text/html",
+  "bool"     : "text/boolean",
+  "turtle"   : "application/x-turtle",
+  "ttl"      : "application/x-turtle",
+  "rdf/xml"  : "application/rdf+xml",
+  "rdfxml"   : "application/rdf+xml",
+  "rdfjson"  : "application/rdf+json",
+  "rdfbin"   : "application/x-binary-rdf",
+  "rdfbint"  : "application/x-binary-rdf-results-table",
+  "js"       : "application/javascript",
+};
 
 (async () => {
   let sparql;
@@ -28,5 +52,17 @@ if (program.args.length < 1 && process.stdin.isTTY) {
   }
   sparql = sparql.toString();
 
-  console.log(sparql);
+  const accept = acceptHeaderMap[opts.format];
+  const headers = {
+    'User-agent': `sparql-client/${version}`,
+    Accept: accept
+  };
+  let requestParams = new URLSearchParams({ 'query': sparql });
+  response = await axios.get(opts.endpoint, { params: requestParams, headers: headers });
+  if (opts.format === 'json') {
+    body = JSON.stringify(response.data, null, 2);
+  } else{
+      body = response.data;
+  }
+  console.log(body);
 })();
